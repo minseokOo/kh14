d62152ecff27e0556749d8bb2eae0c9bab60eedb
minseokOo <minseok916@gmail.com>
2024-05-22 17:40:28 +0900

    첫 번째 프로젝트 생성

---
+++ day01/src/day01/Hello.java
@@ -0,0 +1,16 @@
+//모든 파일은 위치 정보를 가장 먼저 작성해야 합니다
+package day01;
+
+//클래스는 자바 프로그램의 가장 기본 단위(파일 생성시 파일 이름과 같게 생김)
+public class Hello {
+	
+	//main method는 프로그램에 반드시 있어야 되는 영역(실행영역)
+	public static void main(String[] args) {
+		
+		//화면에 글자를 출력하는 구문
+
+		System.out.println("Hello!");
+	}
+}
+
+// {}괄호로 끝나면 영역표시 ;로 끝나면 실행을 지시
---
+++ day01/src/day01/Information.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class Information {
+
+	public static void main(String[] args) {
+
+		System.out.println("김민석");
+		System.out.println("황인빈 강사");
+		System.out.println("공공데이터 융합 자바개발자 양성과정");
+		
+	}
+
+}
