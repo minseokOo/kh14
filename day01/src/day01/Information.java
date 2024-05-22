175994198c929e014f77b0a615d0542239a06299
minseokOo <minseok916@gmail.com>
2024-05-22 18:05:01 +0900

    교육 과정 출력 제거

--- day01/src/day01/Information.java
+++ day01/src/day01/Information.java
@@ -7,7 +7,7 @@ public class Information {
 		System.out.println("김민석");
 		System.out.println("황인빈 강사");
 		System.out.println("KH정보교육원");
-		System.out.println("공공데이터 융합 자바개발자 양성과정");
+		
 		
 	}
 
