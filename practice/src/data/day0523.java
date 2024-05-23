6434d937be738ea6517b10f1efc9290d5479ac9f
minseokOo <minseok916@gmail.com>
2024-05-23 12:41:51 +0900

    변수활용연습

--- practice/src/data/day0523.java
+++ practice/src/data/day0523.java
@@ -15,6 +15,9 @@ public class day0523 {
 		
 		System.out.println("아이스크림 가격은 " + icecream + "원");
 		System.out.println("사탕 가격은 " + candy + "원");
+		System.out.println("아이스크림 " + iceCount + "개의 가격은" + iceTotal + "원");
+		System.out.println("사탕 " + candyCount + "개의 가격은" + candyTotal + "원");
+		System.out.println("아이스크림과 사탕 가격의 총 합은 " + total + "원");
 		
 
 	}
