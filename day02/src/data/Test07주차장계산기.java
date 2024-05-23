54ca2ff583281d547c7686ac428eb103c45907d9
minseokOo <minseok916@gmail.com>
2024-05-23 15:02:10 +0900

    주차장 계산기

---
+++ day02/src/data/Test07주차장계산기.java
@@ -0,0 +1,32 @@
+package data;
+
+public class Test07주차장계산기 {
+
+	public static void main(String[] args) {
+		
+		int hour = 60;
+		int hour1 = 12;
+		int min1 = 40;
+		int hour2 = 15;
+		int min2 = 10;
+		
+		// (1) 이 차량의 주차 시간을 구하여 화면에 출력하시오
+			int totalHour = (hour2 - hour1);
+			int totalMin = (min2 - min1);
+			int totalMin2 = (totalHour * hour + totalMin );
+			int finalHour = (totalMin2 / hour);
+			int finalMin = (totalMin2 % hour);
+			
+			System.out.println("주차 시간은 " + finalHour + "시간 " + finalMin + "분 입니다.");
+			
+			System.out.println( );
+		// (2) 이 차량의 주차 요금을 구하여 화면에 출력하시오.
+			int price = 1000;
+			int mintime = 10;
+			int totalPrice = ((totalMin2 / mintime) * price);
+			
+			System.out.println("총 요금은 " + totalPrice + "원 입니다.");
+					
+	}
+
+}
