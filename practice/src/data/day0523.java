6f165f72b33b9d4a6ae1cfefe7dc2c44c76859ad
minseokOo <minseok916@gmail.com>
2024-05-23 12:30:33 +0900

    연습

---
+++ practice/src/data/day0523.java
@@ -0,0 +1,22 @@
+package data;
+
+public class day0523 {
+
+	public static void main(String[] args) {
+		int icecream = 500;
+		int candy = 200;
+		int iceCount = 3;
+		int candyCount = 5;
+		
+		
+		int iceTotal = (icecream * iceCount);
+		int candyTotal = (candy * candyCount);
+		int total = (iceTotal + candyTotal);
+		
+		System.out.println("아이스크림 가격은 " + icecream + "원");
+		System.out.println("사탕 가격은 " + candy + "원");
+		
+
+	}
+
+}
