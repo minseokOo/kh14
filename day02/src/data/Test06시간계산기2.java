a1efd443157f3e6f2dfd6f861c38bf16b9ac0858
minseokOo <minseok916@gmail.com>
2024-05-23 14:36:47 +0900

    시간 계산기

--- day02/src/data/Test06시간계산기2.java
+++ day02/src/data/Test06시간계산기2.java
@@ -1,6 +1,6 @@
 package data;
 
-public class Test06시간계산기 {
+public class Test06시간계산기2 {
 	public static void main(String[] args) {
 
 // (1)		750분이 몇시간 몇분인지 구하여 출력하시오.
@@ -17,13 +17,11 @@ public class Test06시간계산기 {
 		int c = 3; //두번째 시간
 		int d = 25; //두번째 분
 		
-		int aMin = (a * hour);
-		int cMin = (c * hour);
-		int totalTime = (aMin + cMin + b + d);
-		int totalHour = (totalTime / hour);
-		int totalMin = (totalTime % hour);
+		int m3 = (b + d);
+		int m4 = (m3 / hour);
+		int m5 = (m3 % hour);
 		
-		System.out.println(a + "시간 " + b +"분 + " + c + "시간 " + d + "분을 더하면?");
-		System.out.println("두 시간의 합은 " + totalHour + "시간" + totalMin + "분 입니다.");
+		
+		System.out.println((a + c + m4) + "시간 " + m5 + "분");
 	}
 }
