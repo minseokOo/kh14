3832bdc89e228a8019351722c96518909533cac3
minseokOo <minseok916@gmail.com>
2024-05-23 12:03:56 +0900

    변수 활용 문제

--- day02/src/data/Test03영화금액계산기.java
+++ day02/src/data/Test03영화금액계산기.java
@@ -13,19 +13,15 @@ public class Test03영화금액계산기 {
 		int studentTotal = (student * studentCount);
 		int total = (adultTotal + studentTotal);
 		
-		System.out.println("성인요금");
-		System.out.println(adult);
-		System.out.println("학생요금");
-		System.out.println(student);
+
+		System.out.println("성인요금 : " + adult + "원");
+		System.out.println("학생요금 : " + student + "원");
 		System.out.println( );
-		System.out.println("성인요금합계");
-		System.out.println(adultTotal);
+		System.out.println("성인요금합계 : " + adultTotal + "원");
 		System.out.println( );
-		System.out.println("학생요금합계");
-		System.out.println(studentTotal);
+		System.out.println("학생요금합계 : " + studentTotal + "원");
 		System.out.println( );
-		System.out.println("총 요금");
-		System.out.println(total);
+		System.out.println("총 요금 : " + total + "원");
 		
 	}
 
