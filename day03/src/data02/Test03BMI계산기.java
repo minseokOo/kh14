7f0f1bacc32a72b2ef6a8d7f3fed69491bed83f6
minseokOo <minseok916@gmail.com>
2024-05-24 12:34:44 +0900

    bmi계산기

--- day03/src/data02/Test03BMI계산기.java
+++ day03/src/data02/Test03BMI계산기.java
@@ -4,12 +4,13 @@ public class Test03BMI계산기 {
 
 	public static void main(String[] args) {
 		
-		int height = 180;
-		double mHeight = (double)height / 100;
-		int weight = 80;
-		double height2 = (double)mHeight * mHeight;
+		int height = 180; //키
+		double mHeight = (double)height / 100; //cm를 m로 변환
+		int weight = 80; //몸무게
+		double height2 = (double)mHeight * mHeight; //키(m)의 제곱
 		
 		double bmi = weight / height2;
+		
 		
 		System.out.println("신장 " + height + "cm 몸무게 " + weight + "kg의 BMI지수는 " + bmi + "입니다.");
 	}
