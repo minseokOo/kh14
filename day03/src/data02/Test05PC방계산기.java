b32d5cde42c8e001ab8247a4eb143f3017a7d806
minseokOo <minseok916@gmail.com>
2024-05-24 13:58:25 +0900

    pc방 계산기

--- day03/src/data02/Test05PC방계산기.java
+++ day03/src/data02/Test05PC방계산기.java
@@ -12,6 +12,8 @@ public class Test05PC방계산기 {
 		int endTimeHour = 15; //끝나는 시간
 		int endTimeMinute = 30;
 		int endTime = endTimeHour * 60 + endTimeMinute;
+		
+		//계산
 		int usedTime = endTime - startTime; //이용 시간
 		
 		double minutePrice = (double) hourPrice / 60; //분당 요금
@@ -20,9 +22,12 @@ public class Test05PC방계산기 {
 		
 		int aFinPrice = (int)finPrice; //최종 가격 (int)
 		
-		int bFinPrice = ( aFinPrice / 10 ) * 10; //최종 가격 (반올림)
+		int bFinPrice = ( aFinPrice / 10 ) * 10; //최종 가격
 		
+		int time = usedTime / 60; //이용시간
+		int minute = usedTime % 60;
 		
+		System.out.println("총 이용 시간은 " + time + "시간 " + minute + "분 입니다.");
 		System.out.println("피시방 사용 요금은 " + bFinPrice + "원 입니다.");
 		
 	}
