297ede9fa63f7ebd594c22c56f1c33d3bf267848
minseokOo <minseok916@gmail.com>
2024-05-27 13:49:22 +0900

    0527평균계산기

--- day04/src/data4/Test03평균계산기.java
+++ day04/src/data4/Test03평균계산기.java
@@ -8,5 +8,20 @@ public class Test03평균계산기 {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("국어점수 : ");
+		int korean = sc.nextInt();
+		System.out.println("영어점수 : ");
+		int english = sc.nextInt();
+		System.out.println("수학점수 : ");
+		int math = sc.nextInt();
+		
+		int total = (korean + english + math);
+		System.out.println("총점 : " + total + "점");
+		
+		double avg = (double)total / 3;
+		
+		System.out.println("평균 : " + avg + "점");
+		
+		
 	}
 }
