4a366268321f09920910cc6071e8abf07fc40ff2
minseokOo <minseok916@gmail.com>
2024-05-27 18:04:09 +0900

    Test05지하철 요금 계산기

---
+++ day04/src/condition/Test05지하철요금계산기.java
@@ -0,0 +1,43 @@
+package condition;
+
+import java.util.Scanner;
+
+public class Test05지하철요금계산기 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("출생년도 : ");
+		Scanner sc = new Scanner(System.in);
+		
+		int birth = sc.nextInt();
+		int now = 2024;
+		
+		int age = now - birth +1;
+		System.out.println("나이 : " + age + "세");
+		
+		int adultPrice = 1400;
+		int teenagePrice = 800;
+		int childrenPrice = 500;
+		
+		
+		if (age >= 20 && age < 65) { //성인
+			System.out.println(adultPrice + 500 + "원 입니다.");
+		}
+			else if (age >= 14 && age < 20) { //청소년
+			System.out.println(teenagePrice + 500 + "원 입니다.");	
+			}
+				else if (age >= 8 && age < 14) { //어린이
+				System.out.println(childrenPrice + 500 + "원 입니다.");
+				}
+				else
+				{System.out.println("지하철 요금은 500원 입니다");
+				
+		}
+		
+		
+		
+		
+		
+		
+	}
+}
