ebebb3d4a2b84cbdf2ccc349d009038d789a4b89
minseokOo <minseok916@gmail.com>
2024-05-29 10:48:29 +0900

    Test01반복문

---
+++ day06/src/loop/Test01반복문2.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class Test01반복문2 {
+
+	public static void main(String[] args) {
+//		(Q) Hello World 를 5번 출력하세요.
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+	}
+}
