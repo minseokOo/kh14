8c26643642bbeccfa197cb10b99132c2718aaf10
minseokOo <minseok916@gmail.com>
2024-05-29 13:31:21 +0900

    test03이름입력기

--- day06/src/loop/Test03이름입력기.java
+++ day06/src/loop/Test03이름입력기.java
@@ -9,6 +9,7 @@ public class Test03이름입력기 {
 		Scanner sc = new Scanner(System.in);
 		
 		for(int i = 0; i < 5; i++) {
+		System.out.print("사용자 이름 : ");
 		String name = sc.next();
 		System.out.println(name + "님 환영합니다!");
 		}
