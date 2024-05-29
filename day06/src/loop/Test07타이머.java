10f5d82d3dd9ae15d518787b17d4f525d9ca4b95
minseokOo <minseok916@gmail.com>
2024-05-29 15:57:03 +0900

    test07타이머

--- day06/src/loop/Test07타이머.java
+++ day06/src/loop/Test07타이머.java
@@ -17,11 +17,11 @@ public class Test07타이머 {
 	// second가 0이 됐을때 minute -1 을 하고 second를 59로 만듬
 	// second와 minute가 0이 됐을때 시간이 다 되었습니다!
 	for(int i = total; i >= 0; i--) { // second는 1초씩 줄어듬
-		if (i > 60) {
+		if (i < 60) {
 			int min = i / 60;
-			int sec = i % 60;
-			
+			int sec = i % 60;	
 			System.out.println(min+ "분" + sec + "초");
+			
 		}
 		
 		}
