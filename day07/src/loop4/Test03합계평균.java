67af78306d3d6c73a040dadd1872060ddfdc86ce
minseokOo <minseok916@gmail.com>
2024-05-30 13:49:15 +0900

    test03합계평균

--- day07/src/loop4/Test03합계평균.java
+++ day07/src/loop4/Test03합계평균.java
@@ -6,20 +6,22 @@ public class Test03합계평균 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		int total = 0;
-		
-		
-		while(true) {
+		int count = 0;
+
+		while (true) {
 			System.out.print("숫자 입력 : ");
 			int a = sc.nextInt();
 			total += a;
-			
-			if(a == 0) {
+			count++;
+
+			if (a == 0) {
 				break;
 			}
-			
+
 		}
-		int average = total /3;
+		float average = (float) total / (count - 1);
 		System.out.println("여태까지 입력한 숫자의 합계 : " + total);
 		System.out.println("여태까지 입력한 숫자의 평균 : " + average);
 	}
