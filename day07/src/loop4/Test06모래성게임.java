510fa4cbfe83af123b4dd3d9c9bb27a6f057f8f2
minseokOo <minseok916@gmail.com>
2024-05-30 16:48:42 +0900

    모래성게임

--- day07/src/loop4/Test06모래성게임.java
+++ day07/src/loop4/Test06모래성게임.java
@@ -9,6 +9,25 @@ public class Test06모래성게임 {
 		Scanner sc = new Scanner(System.in);
 		
 		int total = 100;
+		System.out.println("현재 남은 숫자 : " + total);
+		
+		while(true) {
+			System.out.print("숫자 입력(1~9) : ");
+			int a = sc.nextInt();
+			if(a>9 || a<1) {
+				System.out.println("1~9 사이의 수를 입력하세요.");
+				total += a;
+			}
+			if((total-a)<=0) {
+				System.out.println("패배하셨습니다.");
+				break;
+			}
+			total -= a;
+			System.out.println("현재 남은 숫자 : " + total);
+
+			
+			
+		}
 		
 		
 	}
