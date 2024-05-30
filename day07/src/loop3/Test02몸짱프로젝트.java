6cfb1366c62b17701f6d685be00573151e04e7a1
minseokOo <minseok916@gmail.com>
2024-05-30 10:38:31 +0900

    Test02몸짱프로젝트

--- day07/src/loop3/Test02몸짱프로젝트.java
+++ day07/src/loop3/Test02몸짱프로젝트.java
@@ -5,11 +5,12 @@ public class Test02몸짱프로젝트 {
 	public static void main(String[] args) {
 
 		int pushUp = 1; // 첫날 푸쉬업 횟수
-
+		int plus = 3;
+		
 		for (int i = 1; i <= 30; i++) { // 그 날 한 횟수
 
-			pushUp += 3;
-			System.out.println(i + "일차 : " + (pushUp - 3) + "회");
+			pushUp += plus;
+			System.out.println(i + "일차 : " + (pushUp - plus) + "회");
 		}
 
 	}
