e09a6444b53606a79775a936cbdb2593a1bfcd88
minseokOo <minseok916@gmail.com>
2024-05-31 18:20:44 +0900

    선택정렬

---
+++ day08/src/array02/Test01선택정렬.java
@@ -0,0 +1,19 @@
+package array02;
+
+public class Test01선택정렬 {
+
+	public static void main(String[] args) {
+
+		int numbers[] = new int[] {30, 50, 20, 10, 40};
+		int first = 0;
+		
+		for(int i = 0; i < numbers.length; i++) {
+			if (numbers[first] < numbers[first+1]) {
+			}
+			System.out.println(numbers[first]);
+			
+		}
+		
+	}
+
+}
