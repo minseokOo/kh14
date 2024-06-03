29a274c0f88227ae0817fd1bc57b98ad9eb4009d
minseokOo <minseok916@gmail.com>
2024-06-03 09:26:42 +0900

    Test01선택정렬

--- day08/src/array02/Test01선택정렬.java
+++ day08/src/array02/Test01선택정렬.java
@@ -4,17 +4,24 @@ public class Test01선택정렬 {
 
 	public static void main(String[] args) {
 
-		int numbers[] = new int[] {30, 50, 20, 10, 40};
+		int numbers[] = new int[] { 30, 50, 20, 10, 40 };
 		int first = 0;
-		
-		for(int i = 0; i < numbers.length; i++) {
-			if (numbers[first] < numbers[first+1]) {
+
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				if (numbers[i]>numbers[j]) {
+					first = numbers[i];
+					numbers[i] = numbers [j];
+					numbers[j] = first;
+					
+				}
+				
+				
 			}
-			
-			
+
+			System.out.println(numbers[i]);
 		}
-		System.out.println(numbers[first]);
-		
+
 	}
 
 }
