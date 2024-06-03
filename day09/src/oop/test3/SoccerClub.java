51e1a1a2b38ae28d2d64b7597258aa1903d77048
minseokOo <minseok916@gmail.com>
2024-06-03 18:16:35 +0900

    0603

---
+++ day09/src/oop/test3/SoccerClub.java
@@ -0,0 +1,11 @@
+package oop.test3;
+
+public class SoccerClub {
+
+	String name;
+	int match;
+	int win;
+	int draw;
+	int lose;
+	
+}
---
+++ day09/src/oop/test3/Test01프리미어리그.java
@@ -0,0 +1,74 @@
+package oop.test3;
+
+public class Test01프리미어리그 {
+
+	public static void main(String[] args) {
+
+		SoccerClub manCity = new SoccerClub();
+		manCity.name = "맨시티";
+		manCity.match = 38;
+		manCity.win = 28;
+		manCity.draw = 7;
+		manCity.lose = 3;
+		
+		System.out.println(manCity.name);
+		System.out.println(manCity.match);
+		System.out.println(manCity.win);
+		System.out.println(manCity.draw);
+		System.out.println(manCity.lose);
+		
+		SoccerClub arsenal = new SoccerClub();
+		arsenal.name = "아스널";
+		arsenal.match = 38;
+		arsenal.win = 28;
+		arsenal.draw = 5;
+		arsenal.lose = 5;
+		
+		System.out.println(arsenal.name);
+		System.out.println(arsenal.match);
+		System.out.println(arsenal.win);
+		System.out.println(arsenal.draw);
+		System.out.println(arsenal.lose);
+		
+		SoccerClub riverPool = new SoccerClub();
+		riverPool.name = "리버풀";
+		riverPool.match = 38;
+		riverPool.win = 24;
+		riverPool.draw = 10;
+		riverPool.lose = 4;
+		
+		System.out.println(riverPool.name);
+		System.out.println(riverPool.match);
+		System.out.println(riverPool.win);
+		System.out.println(riverPool.draw);
+		System.out.println(riverPool.lose);
+		
+		SoccerClub aston = new SoccerClub();
+		aston.name = "애스턴빌라";
+		aston.match = 38;
+		aston.win = 20;
+		aston.draw = 8;
+		aston.lose = 10;
+		
+		System.out.println(aston.name);
+		System.out.println(aston.match);
+		System.out.println(aston.win);
+		System.out.println(aston.draw);
+		System.out.println(aston.lose);
+		
+		SoccerClub tottenham = new SoccerClub();
+		tottenham.name = "토트넘";
+		tottenham.match = 38;
+		tottenham.win = 20;
+		tottenham.draw = 6;
+		tottenham.lose = 12;
+		
+		System.out.println(tottenham.name);
+		System.out.println(tottenham.match);
+		System.out.println(tottenham.win);
+		System.out.println(tottenham.draw);
+		System.out.println(tottenham.lose);
+		
+	}
+
+}
