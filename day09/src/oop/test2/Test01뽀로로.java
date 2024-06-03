b649379d07ba144fdace1de0d9ca7d74b69f1cfa
minseokOo <minseok916@gmail.com>
2024-06-03 18:00:47 +0900

    0603

--- day09/src/oop/test2/Test01뽀로로.java
+++ day09/src/oop/test2/Test01뽀로로.java
@@ -9,6 +9,10 @@ public class Test01뽀로로 {
 		pororo.character = "뽀로로";
 		pororo.grade = 1;
 		pororo.score = 75;
+
+		System.out.println(pororo.character);
+		System.out.println(pororo.grade);
+		System.out.println(pororo.score);
 		
 		Student crong = new Student();
 		
@@ -16,15 +20,28 @@ public class Test01뽀로로 {
 		crong.grade = 1;
 		crong.score = 50;
 		
+		System.out.println(crong.character);
+		System.out.println(crong.grade);
+		System.out.println(crong.score);
+		
 		Student poby = new Student();
 		
 		poby.character = "포비";
 		poby.grade = 2;
 		poby.score = 86;
 		
-		Student hary = new Student();
+		System.out.println(poby.character);
+		System.out.println(poby.grade);
+		System.out.println(poby.score);
 		
+		Student harry = new Student();
+		harry.character = "해리";
+		harry.grade = 2;
+		harry.score = 77;
 		
+		System.out.println(harry.character);
+		System.out.println(harry.grade);
+		System.out.println(harry.score);
 		
 	}
 
