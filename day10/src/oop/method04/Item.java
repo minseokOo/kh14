6e3df14fcac70d9efe7132ee86f3aa38417db533
minseokOo <minseok916@gmail.com>
2024-06-04 14:08:53 +0900

    온라인쇼핑몰

--- day10/src/oop/method04/Item.java
+++ day10/src/oop/method04/Item.java
@@ -7,6 +7,7 @@ public class Item {
 	int price;
 	int count;
 	boolean discount;
+	int rate;
 	boolean delivery;
 	
 	void setting (String name, String type, int price, int count,
@@ -20,23 +21,31 @@ public class Item {
 		
 	}
 	void setting (String name, String type, int price, int count,
-			boolean discount, boolean delivery) {
+			boolean discount, boolean delivery, int rate) {
 		this.name = name;
 		this.type = type;
 		this.price = price;
 		this.count = count;
 		this.discount = discount;
 		this.delivery = delivery;
+		this.rate = rate;
 		
 	}
 	
 	
 	void info() {
+		System.out.println("<상품정보>");
 		System.out.println(this.name);
 		System.out.println(this.type);
-		System.out.println(this.price);
-		System.out.println(this.count);
-		System.out.println(this.discount);
-		System.out.println(this.delivery);
+		System.out.println("가격 : " + this.price + "원");
+		System.out.println("개수 : " + this.count + "개");
+		if(discount) {
+		System.out.print(this.price * (100 - this.rate) / 100 +"원");
+		System.out.println(" (원가 : " + this.price + "원)");}
+		if(delivery) {
+		System.out.println("새벽배송 가능");}
+		else {
+			System.out.println("새벽배송 불가능");
+		}
 	}
 }
--- day10/src/oop/method04/Test01온라인쇼핑몰.java
+++ day10/src/oop/method04/Test01온라인쇼핑몰.java
@@ -11,8 +11,8 @@ public class Test01온라인쇼핑몰 {
 		
 		a.setting("비김면", "라면", 16800, 2, true);
 		b.setting("크림대빵", "제과", 6500, 2, false);
-		c.setting("점보도시락", "라면", 8500, 3, true, true);
-		d.setting("공간춘", "라면", 12300, 3, true, false);
+		c.setting("점보도시락", "라면", 8500, 3, true, true, 5);
+		d.setting("공간춘", "라면", 12300, 3, true, false, 20);
 		
 		
 		a.info();
