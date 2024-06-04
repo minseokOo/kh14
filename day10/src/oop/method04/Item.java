863ac498305d683e740670201846ef5421cb8c2f
minseokOo <minseok916@gmail.com>
2024-06-04 15:02:35 +0900

    온라인 쇼핑몰

--- day10/src/oop/method04/Item.java
+++ day10/src/oop/method04/Item.java
@@ -12,13 +12,14 @@ public class Item {
 	
 	void setting (String name, String type, int price, int count,
 			boolean delivery) {
-		this.name = name;
-		this.type = type;
-		this.price = price;
-		this.count = count;
-		this.discount = false;
-		this.delivery = delivery;
+//		this.name = name;
+//		this.type = type;
+//		this.price = price;
+//		this.count = count;
+//		this.discount = false;
+//		this.delivery = delivery;
 		
+		this.setting(name, type, price, count, false, delivery, 0);
 	}
 	void setting (String name, String type, int price, int count,
 			boolean discount, boolean delivery, int rate) {
@@ -37,15 +38,20 @@ public class Item {
 		System.out.println("<상품정보>");
 		System.out.println(this.name);
 		System.out.println(this.type);
-		System.out.println("가격 : " + this.price + "원");
-		System.out.println("개수 : " + this.count + "개");
 		if(discount) {
-		System.out.print(this.price * (100 - this.rate) / 100 +"원");
-		System.out.println(" (원가 : " + this.price + "원)");}
+			int resultPrice = this.price * (100 - this.rate) / 100;
+			System.out.print("가격 : " + resultPrice +"원");
+			System.out.println(" (원가 : " + this.price + "원)");
+			System.out.println("총 가격 : " + resultPrice * this.count + "원");}
+		else {
+		System.out.println("가격 : " + this.price + "원");}
+		System.out.println("개수 : " + this.count + "개");
+		System.out.println("총 가격 : " + this.price * this.count + "원");
 		if(delivery) {
 		System.out.println("새벽배송 가능");}
 		else {
 			System.out.println("새벽배송 불가능");
 		}
+		System.out.println("-------------------");
 	}
 }
