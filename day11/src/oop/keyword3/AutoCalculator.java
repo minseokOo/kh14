dfdf32b049068dab15d51cdf3062bc046bb42e1e
minseokOo <minseok916@gmail.com>
2024-06-05 16:04:13 +0900

    auto calculator

--- day11/src/oop/keyword3/AutoCalculator.java
+++ day11/src/oop/keyword3/AutoCalculator.java
@@ -8,15 +8,18 @@ public class AutoCalculator {
 	public static int multiple(int a, int b) {
 		return a * b;
 	}
+	public static double multiple(double a, double b) {
+		return a * b;
+	}
 	public static int square(int a) {
 		return a*a;
 	}
 	public static float circle(int a) {
-		return a * a * 3.14f;
+		return square(a) * 3.14f;
 	}
 	public static int random(int a, int b) {
 		Random r = new Random();
-		return r.nextInt(b-a)+a;
+		return r.nextInt(b-a+1)+a;
 	}
 	public static int lotto() {
 		Random r = new Random();
