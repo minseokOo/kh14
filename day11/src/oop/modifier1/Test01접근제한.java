3799bfb9f8509c0351d3ab878b09dbbcc83d6057
minseokOo <minseok916@gmail.com>
2024-06-05 11:02:17 +0900

    접근제한 키워드

---
+++ day11/src/oop/modifier1/Test01접근제한.java
@@ -0,0 +1,14 @@
+package oop.modifier1;
+
+public class Test01접근제한 {
+
+	public static void main(String[] args) {
+		
+		Student st1 = new Student();
+		
+		st1.setting("피카츄", 77);
+//		st1.setScore(101);
+//		st1.score = 500;
+		st1.info();
+	}
+}
