93c129594dcbabc209f5c1344d69c78398e317ea
minseokOo <minseok916@gmail.com>
2024-06-10 15:48:58 +0900

    상속주의사항

---
+++ day12/src/oop/inherit07/Test01상속주의사항.java
@@ -0,0 +1,11 @@
+package oop.inherit07;
+
+public class Test01상속주의사항 {
+	public static void main(String[] args) {
+		
+		GalaxyFold4 phone = new GalaxyFold4();
+		phone.serial();
+		phone.samsungPay();
+		
+	}
+}
