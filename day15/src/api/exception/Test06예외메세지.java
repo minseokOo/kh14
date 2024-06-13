567084a4d1c223528dbf2bc6e88b2af3e84d6cad
minseokOo <minseok916@gmail.com>
2024-06-13 16:15:06 +0900

    예외메세지 처리

--- day15/src/api/exception/Test06예외메세지.java
+++ day15/src/api/exception/Test06예외메세지.java
@@ -14,25 +14,27 @@ public class Test06예외메세지 {
 		//문제가 될 경우 catch 블록으로 이동시키도록 코드를 작성
 		if(money <= 0) {
 			//catch 로 보냄
-			throw new Exception();
+			//throw new Exception();
+			throw new Exception("금액은 0보다 커야합니다.");//메세지를 갖고 catch 블록으로 보냄
 		}
 		
 		System.out.print("인원 : ");
 		int people = sc.nextInt();
-		if(people < 0) {
+		if(people <= 1) {
 			//catch 로 보냄
-			throw new Exception();
+			throw new Exception("인원은 2명 이상이어야 합니다.");
 		}
 		int result = money / people;
 		System.out.println("한 사람당 " + result + "원을 내야합니다.");
 		}
-		//catch(Object e){//안됨
-		//catch(Throwable e) { 너무 큰 범위를 포함
-		catch(Exception e) { // 자바의 모든 예외를 처리
-		//catch(RuntimeException e){//자바의 일부 예외
-			System.err.println("0 보다 큰 숫자만 입력하세요.");
+		catch(Exception e) {//null 이 나오면
+			if(e.getMessage() == null) {
+				System.err.println("오류가 발생했습니다.");
+			}
+			else {//메세지가 있으면
+				System.err.println(e.getMessage());
+			}
 		}
-		
 		
 	}
 }
