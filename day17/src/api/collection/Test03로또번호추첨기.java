35aaf55b4e3bbb6613e0dd90eda2a36f5889f731
minseokOo <minseok916@gmail.com>
2024-06-17 11:09:34 +0900

    로또번호 추첨기

--- day17/src/api/collection/Test03로또번호추첨기.java
+++ day17/src/api/collection/Test03로또번호추첨기.java
@@ -10,7 +10,13 @@ public class Test03로또번호추첨기 {
 		ArrayList<Integer> a = new ArrayList<>(6);
 		
 		for(int i=0; i<6; i++) {
+		if(a.contains(a)){ // 이미 존재하는 번호라면
+			i--;
+		}
+		else { //존재하지 않는 번호라면
 		a.add(r.nextInt(45)+1);
+		
+		}
 		}
 		System.out.println("로또번호 = " + a);
 	}
