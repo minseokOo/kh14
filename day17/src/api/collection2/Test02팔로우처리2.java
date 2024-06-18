445bd1c037dcb69d70d436ee917315a5eee8559f
minseokOo <minseok916@gmail.com>
2024-06-18 10:01:47 +0900

    팔로우 시스템 구현

--- day17/src/api/collection2/Test02팔로우처리2.java
+++ day17/src/api/collection2/Test02팔로우처리2.java
@@ -1,10 +1,11 @@
 package api.collection2;
 
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class Test02팔로우처리 {
+public class Test02팔로우처리2 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -18,7 +19,7 @@ public class Test02팔로우처리 {
 			if (id.equals(exit)) {
 				System.out.println("프로그램을 종료합니다");
 				sc.close();
-				System.out.println("팔로우 목록 : " + data);
+//				System.out.println("팔로우 목록 : " + data);
 				System.out.println("총 팔로우 수 : " + data.size() + "명");
 				break;
 			} else if (!data.contains(id)) {
@@ -29,6 +30,18 @@ public class Test02팔로우처리 {
 				System.out.println(id + "를 팔로우 해제했습니다.");
 			}
 
+		}
+		
+		// 팔로우 리스트 출력(=저장소에 있는 값 출력)
+		// 1. 확장 FOR 사용
+		System.out.println("<팔로우 목록>");
+		for(String id : data) {
+			System.out.println("ID : " + id);
+		}
+		// 2. Iterator 사용
+		Iterator<String>iterator = data.iterator();
+		while(iterator.hasNext()) {
+			System.out.println("ID : " + iterator.next());
 		}
 	}
 }
