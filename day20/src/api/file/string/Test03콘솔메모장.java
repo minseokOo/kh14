36f9562fa99a23ebf53f628b7e0bcf82744823fd
minseokOo <minseok916@gmail.com>
2024-06-20 16:09:30 +0900

    콘솔메모장

---
+++ day20/src/api/file/string/Test03콘솔메모장.java
@@ -0,0 +1,41 @@
+package api.file.string;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Test03콘솔메모장 {
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		File dir = new File("files");
+		File target = new File("./files", "notepad.kh");
+		File temp = File.createTempFile("notepad", ".kh", dir); //임시파일
+		
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(temp)));
+		
+		while(true) {
+			String input = sc.nextLine();
+			if(input.equals("종료"))
+				break;
+			
+			else{
+			pw.println(input);
+			pw.flush();
+			}
+		}
+		sc.close();
+		pw.close();
+		
+		FileInputStream in = new FileInputStream(temp);
+		FileOutputStream out = new FileOutputStream(target);
+		byte[] buffer = new byte[8192];
+		
+	}
+}
