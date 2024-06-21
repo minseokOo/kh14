94289bc06262aebb1a2513782a75e529dcdf2ec9
minseokOo <minseok916@gmail.com>
2024-06-21 10:01:36 +0900

    Create 01.member테이블.sql

---
+++ database/01.member테이블.sql
@@ -0,0 +1,9 @@
+create table member(
+    member_id varchar2(20),
+    member_pw varchar2(16),
+    member_nickname varchar2(30),
+    member_birth char(10),
+    member_contact char(13)
+    );
+
+drop table member;
