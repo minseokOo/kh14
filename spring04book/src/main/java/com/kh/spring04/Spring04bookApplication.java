c816fa887bba4749b966a5ba489026dc39bc35a3
minseokOo <minseok916@gmail.com>
2024-06-28 17:50:18 +0900

    0628과제

---
+++ spring04book/src/main/java/com/kh/spring04/Spring04bookApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring04bookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring04bookApplication.class, args);
+	}
+
+}
---
+++ spring04book/src/test/java/com/kh/spring04/Spring04bookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring04bookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
