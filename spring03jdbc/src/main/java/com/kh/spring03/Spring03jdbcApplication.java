d07b68c8cb9e38fd1ff6410cff3244a87af9d663
minseokOo <minseok916@gmail.com>
2024-06-28 10:27:43 +0900

    spring03 생성(강사)

---
+++ spring03jdbc/src/main/java/com/kh/spring03/Spring03jdbcApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring03jdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring03jdbcApplication.class, args);
+	}
+
+}
---
+++ spring03jdbc/src/test/java/com/kh/spring03/Spring03jdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03jdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
