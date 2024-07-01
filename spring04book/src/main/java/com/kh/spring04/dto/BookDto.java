fb0f1e64c276300d910d3786a5d19c8764acac8d
minseokOo <minseok916@gmail.com>
2024-07-01 11:11:09 +0900

    lombok

--- spring04book/src/main/java/com/kh/spring04/dto/BookDto.java
+++ spring04book/src/main/java/com/kh/spring04/dto/BookDto.java
@@ -1,5 +1,8 @@
 package com.kh.spring04.dto;
 
+import lombok.Data;
+
+@Data
 public class BookDto {
 	private int bookId;
 	private String bookTitle;
@@ -11,62 +14,4 @@ public class BookDto {
 	private String bookGenre;
 	
 	
-	
-	@Override
-	public String toString() {
-		return "BookDto [bookId=" + bookId + ", bookTitle=" + bookTitle + ", bookAuthor=" + bookAuthor
-				+ ", bookPublicationDate=" + bookPublicationDate + ", bookPrice=" + bookPrice + ", bookPublisher="
-				+ bookPublisher + ", bookPageCount=" + bookPageCount + ", bookGenre=" + bookGenre + "]";
-	}
-	public BookDto() {
-		super();
-	}
-	public int getBookId() {
-		return bookId;
-	}
-	public void setBookId(int bookId) {
-		this.bookId = bookId;
-	}
-	public String getBookTitle() {
-		return bookTitle;
-	}
-	public void setBookTitle(String bookTitle) {
-		this.bookTitle = bookTitle;
-	}
-	public String getBookAuthor() {
-		return bookAuthor;
-	}
-	public void setBookAuthor(String bookAuthor) {
-		this.bookAuthor = bookAuthor;
-	}
-	public String getBookPublicationDate() {
-		return bookPublicationDate;
-	}
-	public void setBookPublicationDate(String bookPublicationDate) {
-		this.bookPublicationDate = bookPublicationDate;
-	}
-	public int getBookPrice() {
-		return bookPrice;
-	}
-	public void setBookPrice(int bookPrice) {
-		this.bookPrice = bookPrice;
-	}
-	public String getBookPublisher() {
-		return bookPublisher;
-	}
-	public void setBookPublisher(String bookPublisher) {
-		this.bookPublisher = bookPublisher;
-	}
-	public int getBookPageCount() {
-		return bookPageCount;
-	}
-	public void setBookPageCount(int bookPageCount) {
-		this.bookPageCount = bookPageCount;
-	}
-	public String getBookGenre() {
-		return bookGenre;
-	}
-	public void setBookGenre(String bookGenre) {
-		this.bookGenre = bookGenre;
-	}
 }
