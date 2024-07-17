2a65488dd8b18b404ed3a39707d432a3763efdac
minseokOo <minseok916@gmail.com>
2024-07-17 15:50:42 +0900

    01.member테이블.sql 업데이트

--- database/01.member테이블.sql
+++ database/01.member테이블.sql
@@ -7,3 +7,30 @@ create table member(
     );
 
 drop table member;
+
+```sql
+drop sequence board_seq;
+create sequence board_seq;
+
+drop table board;
+create table board (
+board_no number primary key,
+board_title varchar2(300 byte) not null,
+board_content varchar2(4000 byte) not null,
+board_writer references member(member_id) on delete set null,
+board_wtime date default sysdate not null,
+board_utime date,
+board_views number default 0 not null,
+board_likes number default 0 not null,
+board_replies number default 0 not null,
+board_group number not null,
+board_target references board(board_no) on delete cascade,
+board_depth number not null,
+check(board_no > 0),
+check(board_views >= 0),
+check(board_likes >= 0),
+check(board_replies >= 0),
+check(board_group > 0),
+check(board_depth >= 0)
+);
+```
