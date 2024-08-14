15ae3060924a0fd1ddfc0ab9fb532a3accf10736
minseokOo <minseok916@gmail.com>
2024-08-14 10:13:04 +0900

    spring09email 생성

---
+++ spring09email/src/main/java/com/kh/spring09/Spring09emailApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09emailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring09emailApplication.class, args);
+	}
+
+}
