142129ad315ee7923bbeded6bf952c4834215afd
minseokOo <minseok916@gmail.com>
2024-08-14 09:46:02 +0900

    spring09email생성

---
+++ spring09email/src/test/java/com/kh/spring09/Spring09emailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09emailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
