f1ed7e2a73ee670ca39df7e582e19a99ebcb2cb3
minseokOo <minseok916@gmail.com>
2024-09-27 09:45:48 +0900

    새 프로젝트 생성

---
+++ spring12mybatis/src/main/java/com/spring12/mybatis/Spring12mybatisApplication.java
@@ -0,0 +1,13 @@
+package com.spring12.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring12mybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring12mybatisApplication.class, args);
+	}
+
+}
---
+++ spring12mybatis/src/test/java/com/spring12/mybatis/Spring12mybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring12.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring12mybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
